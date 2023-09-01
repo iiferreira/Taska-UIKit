3c0c181276c1d47e93edaade83c2efec138e7f77
Iuri <bokin@me.com>
2023-09-01 09:13:06 -0300

    perf: add table view

---
+++ Taska-UIKit/ViewController+Layout.swift
@@ -0,0 +1,65 @@
+//
+//  ViewController+Layout.swift
+//  Taska-UIKit
+//
+//  Created by Iuri Ferreira on 01/09/23.
+//
+
+import UIKit
+
+extension ViewController {
+    func configureView() {
+        view.addSubview(logo)
+        view.addSubview(itemListLabel)
+        view.addSubview(addItemButton)
+        view.addSubview(tableViewBackGround)
+        view.addSubview(tableView)
+        
+        NSLayoutConstraint.activate([
+            logo.topAnchor.constraint(equalToSystemSpacingBelow: view.safeAreaLayoutGuide.topAnchor, multiplier: 3),
+            logo.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 3),
+            logo.widthAnchor.constraint(equalToConstant: 200),
+            logo.heightAnchor.constraint(equalToConstant: 80),
+        ])
+        
+        NSLayoutConstraint.activate([
+            itemListLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor,constant: 35),
+            itemListLabel.topAnchor.constraint(equalTo: logo.bottomAnchor)
+        ])
+        
+        NSLayoutConstraint.activate([
+            addItemButton.centerYAnchor.constraint(equalTo: logo.centerYAnchor),
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: addItemButton.trailingAnchor, multiplier: 3),
+            addItemButton.widthAnchor.constraint(equalToConstant: 48),
+            addItemButton.heightAnchor.constraint(equalToConstant: 48),
+        ])
+        
+        NSLayoutConstraint.activate([
+            tableViewBackGround.topAnchor.constraint(equalTo: itemListLabel.bottomAnchor, constant: 75),
+            tableViewBackGround.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 50),
+            tableViewBackGround.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -50),
+            view.bottomAnchor.constraint(equalToSystemSpacingBelow: tableViewBackGround.bottomAnchor, multiplier: 20)
+        ])
+        
+        NSLayoutConstraint.activate([
+            tableViewBackGround.topAnchor.constraint(equalTo: itemListLabel.bottomAnchor, constant: 75),
+            tableViewBackGround.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 50),
+            tableViewBackGround.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -50),
+            view.bottomAnchor.constraint(equalToSystemSpacingBelow: tableViewBackGround.bottomAnchor, multiplier: 20)
+        ])
+        
+        tableViewBackGround.addSubview(tableView)
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: tableViewBackGround.topAnchor,constant: 25),
+            tableView.leadingAnchor.constraint(equalTo: tableViewBackGround.leadingAnchor, constant: 6),
+            tableView.trailingAnchor.constraint(equalTo: tableViewBackGround.trailingAnchor, constant: -6),
+            tableView.bottomAnchor.constraint(equalTo: tableViewBackGround.bottomAnchor, constant: -25)
+            
+        ])
+    }
+}
+
+extension UIColor {
+    static let systemBrand = UIColor(named: "brandBackground")
+}
--- Taska-UIKit/ViewController.swift
+++ Taska-UIKit/ViewController.swift
@@ -9,11 +9,75 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let logo = UIImageView()
+    let itemListLabel = UILabel()
+    let addItemButton = UIButton()
+    
+    let tableViewBackGround = UIView()
+    let tableView = UITableView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        style()
+        layout()
+        setupTableView()
+    }
+    
+    private func setupTableView() {
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.rowHeight = 44
+        tableView.showsVerticalScrollIndicator = false
+    }
+    
+    private func style() {
+        view.backgroundColor = .systemBrand
+        
+        logo.translatesAutoresizingMaskIntoConstraints = false
+        itemListLabel.translatesAutoresizingMaskIntoConstraints = false
+        addItemButton.translatesAutoresizingMaskIntoConstraints = false
+        tableViewBackGround.translatesAutoresizingMaskIntoConstraints = false
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        //Logo
+        let logoImage = UIImage(named: "taska")
+        logo.image = logoImage
+        logo.contentMode = .scaleAspectFit
+        
+        //Item List Label
+        itemListLabel.text = "X coisas a fazer"
+        itemListLabel.textColor = .white
+        itemListLabel.font = UIFont.preferredFont(forTextStyle: .headline)
+        
+        //Add button
+        addItemButton.setBackgroundImage(UIImage(systemName: "plus.circle")?.withTintColor(.white, renderingMode: .alwaysOriginal), for: .normal)
+        
+        tableViewBackGround.backgroundColor = .white
+        tableViewBackGround.layer.cornerRadius = 45
+    }
+    
+    private func layout() {
+        configureView()
     }
 
-
 }
+
+//MARK: - Actions uitableview data source and delegate methods
+
+extension ViewController : UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Hello"
+        return cell
+    }
+    
+}
+
+
+
 
