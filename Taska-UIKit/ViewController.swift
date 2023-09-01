0ab5b123f18a9ecc414379a67f4da637b5a2b0a5
Iuri <bokin@me.com>
2023-08-31 22:16:59 -0300

    Initial Commit

---
+++ Taska-UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Taska-UIKit
+//
+//  Created by Iuri Ferreira on 31/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
